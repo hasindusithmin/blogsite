85a31f77efff1ad0c7b305088b32c91d9353ffbe
hasindusithmin <hasindusithmin64@gmail.com>
2022-02-17 20:16:16 +0530

    Add for loop

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 
-//regular function
 
-function myfn(){
-    console.log('====================================');
-    console.log('regular function');
-    console.log('====================================');
-}
-
-//arrow function
-const _myfn = (param1,param2)=>{
-    console.log('====================================');
-    console.log('arrow function');
-    console.log('====================================');
-}
-
-// https://jsonplaceholder.typicode.com/posts
+const url = 'https://jsonplaceholder.typicode.com/posts'
 
 const get_comment = async()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await response.json()
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
-}
 
+    const response = await fetch(url);
+    const posts = await response.json() // data
+
+    // const nodeList = [];
+
+    // <div id={id}>
+    //     <h1>{title}</h1>
+    //     <p>{body}</p>
+    // </div>
+
+    for(post of posts) {
+        const {id,title,body} = post; //destructure
+        //******creating (element) */
+        //create div element
+        const div = document.createElement('div') 
+        //create h1 element
+        const h1 = document.createElement('h1')
+        //create p element
+        const p = document.createElement('p')
+        
+        div.id = id;
+        h1.innerHTML = title;
+        p.innerHTML = body;
+
+        div.appendChild(h1)
+        div.appendChild(p)
+
+        document.getElementById('root').appendChild(div)
+       
+    }
+
+}
+get_comment()
 
 
